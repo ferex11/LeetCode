b4f4cb654415541ad0246439e4911d2f68b392f9
Sujeet Kumar <sujeet901626@gmail.com>
2026-08-17 11:02:08 +0530

    Update and rename 1038-Binary Search Tree to Greater Sum Tree.cpp to 01038 - Binary Search Tree to Greater Sum Tree.cpp

--- 01038 - Binary Search Tree to Greater Sum Tree.cpp
+++ 01038 - Binary Search Tree to Greater Sum Tree.cpp
@@ -4,13 +4,11 @@ public:
         if (!root) {
             return;
         }
-
         solve(root->right, sum);
         sum += root->val;
         root->val = sum;
         solve(root->left, sum);
     }
-
     TreeNode* bstToGst(TreeNode* root) {
         int sum = 0;
         solve(root, sum);
