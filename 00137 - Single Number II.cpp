5bfd5ea9eeaf6f6d4ab99d922bd71eece3a74781
Sujeet Kumar <sujeet901626@gmail.com>
2026-08-19 13:41:56 +0530

    Update and rename 137-Single Number II.cpp to 00137 - Single Number II.cpp

--- 00137 - Single Number II.cpp
+++ 00137 - Single Number II.cpp
@@ -1,26 +1,6 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int, int> mpp;
-
-        for (auto it : nums) {
-            mpp[it]++;
-        }
-
-        for (auto it : mpp) {
-            if (it.second == 1) {
-                return it.first;
-            }
-        }
-        return -1;
-    }
-};
-
-
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
         int res = 0;
         for (int k = 0; k <= 31; k++) {
             int countOnes = 0;
